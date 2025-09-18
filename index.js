39529759a8c60419d3c5191a30c7f0f18d4a03d5
Your Name <you@example.com>
2025-09-18 11:26:56 +0100

    complete the full build for the Home and bout page, including the navbar complication

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+const sideNav = document.getElementById("sideNav");
+
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 500 && window.innerWidth > 100) {
+  if (window.scrollY > 300 && !sideNav.classList.contains("active")) {
     document.querySelector(".nav2").classList.add("surface-nav");
   } else {
     document.querySelector(".nav2").classList.remove("surface-nav");
   }
 });
+
+function startSidebar() {
+  const sideNav = document.getElementById("sideNav");
+  sideNav.classList.add("active");
+  document.querySelector(".nav2").classList.remove("surface-nav");
+}
+
+function endSidebar() {
+  const sideNav = document.getElementById("sideNav");
+  sideNav.classList.remove("active");
+  if (window.scrollY > 400 && window.innerWidth > 100) {
+    document.querySelector(".nav2").classList.add("surface-nav");
+  } else {
+    document.querySelector(".nav2").classList.remove("surface-nav");
+  }
+}
